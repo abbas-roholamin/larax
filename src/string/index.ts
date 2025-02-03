4aee40728708f79e786663c22c591b94177fea53
abbas-roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2025-02-04 00:14:27 +0430

    fix: Fix the Str contains method

--- src/string/index.ts
+++ src/string/index.ts
@@ -1,20 +1,28 @@
+/** @format */
 class Str {
-    static contains(str: string, keys: string[] | string, ignoreCase = false): boolean {
-        if (typeof keys === "string") {
-            keys = [keys];
-        }
-
-        const strSet = new Set(ignoreCase ? str.toLowerCase().split('') : str.split(''));
-
-        return keys.some((key) => {
-            if (key === "") {
-                return false;
-            }
-            const normalizedKey = ignoreCase ? key.toLowerCase() : key;
-            return normalizedKey.split('').some(char => strSet.has(char));
-        });
+  static contains(
+    str: string,
+    keys: string[] | string,
+    ignoreCase = false,
+  ): boolean {
+    if (typeof keys === "string") {
+      keys = [keys]
     }
+
+    const strSet = new Set(
+      ignoreCase ? str.toLowerCase().split(" ") : str.split(" "),
+    )
+
+    return keys.some((key) => {
+      if (key === "") {
+        return false
+      }
+
+      key = ignoreCase ? key.toLowerCase() : key
+
+      return strSet.has(key)
+    })
+  }
 }
 
-
-export default Str;
+export default Str
